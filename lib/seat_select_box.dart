9a42f26e8b90682673db0680c97065025079c0b3
hippieperm <leess0133@gmail.com>
2025-03-31 20:18:39 +0900

    SeatSelectBox 위젯 수정: '1' 텍스트를 추가하고, Row 위젯을 포함하여 UI 개선

--- lib/seat_select_box.dart
+++ lib/seat_select_box.dart
@@ -6,17 +6,32 @@ class SeatSelectBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-        child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          'Screen',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Screen',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ),
-      ],
-    ));
+          Row(
+            children: [
+              Expanded(
+                child: Text(
+                  '1',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
+
