2c6994cf3d85109b2983f91ba0734cc0c6451ad3
hippieperm <leess0133@gmail.com>
2025-04-01 10:50:25 +0900

    SeatPage 위젯 수정: StatelessWidget에서 StatefulWidget으로 변경하여 상태 관리 기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class SeatPage extends StatelessWidget {
+class SeatPage extends StatefulWidget {
   const SeatPage({super.key});
 
+  @override
+  State<SeatPage> createState() => _SeatPageState();
+}
+
+class _SeatPageState extends State<SeatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
