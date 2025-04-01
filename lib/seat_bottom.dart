f6cd825d65dc4fbd73188e2e40f214a43af383d3
hippieperm <leess0133@gmail.com>
2025-04-01 10:45:22 +0900

    SeatBottom 위젯 수정: 선택된 좌석이 없을 때 메시지 추가 및 'Book Now' 버튼 추가로 UI 개선

--- lib/seat_bottom.dart
+++ lib/seat_bottom.dart
@@ -14,7 +14,21 @@ class SeatBottom extends StatelessWidget {
       ),
       child: Column(
         children: [
-          Text('data'),
+          SizedBox(height: 20),
+          Text(
+            '선택된 좌석 없음',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Book Now'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.amber,
+            ),
+          )
         ],
       ),
     );
